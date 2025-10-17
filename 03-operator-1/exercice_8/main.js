5a66772e2812a37dda0aee196b18838dbb3d52c7
Kis-lander <abeltenda.gmail.com>
2025-10-17 12:25:01 +0100

    feat : exercises 8 has been modified

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,22 +1,21 @@
 // Exercice 8
 // Écrivez votre code ici
 
-const salaireMensuel = 500;
+let salaireMensuel = 500;
+      salaireMensuel += 500 * 0,1;
 const loyer = salaireMensuel * 0.3;
 const nourriture = salaireMensuel * 0.2;
 const transport = salaireMensuel * 0.1;
 let autresDepenses = 50;
     autresDepenses += 25;
-let totalDepenses = loyer + nourriture + transport + autresDepenses;
+let totalDepenses = (loyer + nourriture + transport + autresDepenses);
 let reste = salaireMensuel - totalDepenses;
 
 function verifierReste(reste){
     if(reste>=100){
-        console.log('budjet bien gerer');
-        
+        console.log('budjet bien gerer');    
     }else{
-        console.log('attention serrer');
-        
+        console.log('attention serrer');   
     }
 }
 
@@ -29,3 +28,20 @@ function calcultotalDepenses(salaire, depenses){
     }
 }
 calcultotalDepenses(800, 500);
+
+const loisirs = salaireMensuel * 0.15;
+    totalDepenses = loyer + nourriture + transport + autresDepenses + loisirs;
+    reste = salaireMensuel - totalDepenses;
+
+let pourcentageLoyer = (loyer * 100) / salaireMensuel;
+let pourcentageNourriture = (nourriture * 100) / salaireMensuel;
+let pourcentageTransport = (transport * 100) / salaireMensuel;
+let pourcentageAutresDepenses = (autresDepenses * 100) / salaireMensuel;
+let pourcentageLoisirs = (loisirs * 100) / salaireMensuel;
+console.log(pourcentageLoyer);
+console.log(pourcentageNourriture);
+console.log(pourcentageTransport);
+console.log(pourcentageAutresDepenses);
+console.log(pourcentageLoisirs);
+
+
